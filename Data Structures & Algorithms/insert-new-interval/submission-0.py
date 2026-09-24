9ecbf8f7ac240585cf48f655df385b61c6dbd324
damonseto <damon@seto.ca>
2026-09-23 20:02:34 -0700

    Add: insert-new-interval - submission-0

---
+++ Data Structures & Algorithms/insert-new-interval/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+        i = 0
+        n = len(intervals)
+        while i < n and intervals[i][1] < newInterval[0]:
+            res.append(intervals[i])
+            i += 1
+        merged = [newInterval[0], newInterval[1]]
+        while i < n and intervals[i][0] <= newInterval[1]:
+            merged[0] = min(merged[0], intervals[i][0])
+            merged[1] = max(merged[1], intervals[i][1])
+            i += 1
+        res.append(merged)
+        while i < n:
+            res.append(intervals[i])
+            i += 1
+        return res
+        
+        
